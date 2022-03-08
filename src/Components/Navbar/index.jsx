d8c91c740ebcac57c1bb9730d6d673c742a00297
Siddhant Deshmukh <sidtdesh@gmail.com>
2022-03-08 22:35:12 +0530

    -WIP Responsive navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import {MenuOpen} from "@material-ui/icons"
 import "./style.css";
 
 const Navbar = () => {
@@ -19,14 +20,14 @@ const Navbar = () => {
             <Typography className="me-md-4 me-2">About</Typography>
           </span>
 
-          <button onClick={() => setMobileNavOpen(!mobileNavOpen)} className="d-block d-md-none">A</button>
+          <IconButton onClick={() => setMobileNavOpen(!mobileNavOpen)} className="d-block d-md-none"><MenuOpen /></IconButton>
         </Toolbar>
       </AppBar>
 
-      <div className={`mobile_nav ${mobileNavOpen ? 'showMobileNav' : 'hideMobileNav'} d-block d-md-none`}>
-        <Typography className="me-md-4 me-2">Projects</Typography>
-        <Typography className="me-md-4 me-2">Contact</Typography>
-        <Typography className="me-md-4 me-2">About</Typography>
+      <div className={`mobile_nav ${mobileNavOpen ? 'showMobileNav' : 'hideMobileNav'} d-md-none`}>
+        <Typography className="py-4 my-3">Projects</Typography>
+        <Typography className="py-4 my-3">Contact</Typography>
+        <Typography className="py-4 my-3 ">About</Typography>
       </div>
     </>
   );
